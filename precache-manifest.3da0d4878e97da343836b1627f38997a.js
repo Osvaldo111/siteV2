647bcdd6f72fc2d67dee2cf44220232ac047874d
Osvaldo <osvaldofabricio11@gmail.com>
2020-03-18 22:23:26 -0500

    Updates

---
+++ precache-manifest.3da0d4878e97da343836b1627f38997a.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d6f47798acb980d879a2d1683459f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86f789f512f874468375",
+    "url": "/static/css/main.6d0eb022.chunk.css"
+  },
+  {
+    "revision": "e8e413f870d88ae1662d",
+    "url": "/static/js/2.f3224ac3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.f3224ac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86f789f512f874468375",
+    "url": "/static/js/main.9b963881.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "e0e72ba97798f89a079f105803cf19aa",
+    "url": "/static/media/arrow-down.e0e72ba9.svg"
+  },
+  {
+    "revision": "566a8124e5a7b6c50c55422afa960590",
+    "url": "/static/media/cryptoApp.566a8124.svg"
+  },
+  {
+    "revision": "b368e0b4aee332f8a88976163821d7f4",
+    "url": "/static/media/css-icon.b368e0b4.svg"
+  },
+  {
+    "revision": "6daf7518217adc87c5862fb3d653dbb5",
+    "url": "/static/media/exit-icon.6daf7518.svg"
+  },
+  {
+    "revision": "7d6db13fe72214368ac287b75942352c",
+    "url": "/static/media/floppyer-icon.7d6db13f.svg"
+  },
+  {
+    "revision": "f15529f5de2762a14ff1163851049c5e",
+    "url": "/static/media/floppyerApp.f15529f5.svg"
+  },
+  {
+    "revision": "b6d4ca7b246065fd5e1ca90a9deb7fc7",
+    "url": "/static/media/github-icon.b6d4ca7b.svg"
+  },
+  {
+    "revision": "cb5e5ee737d85f9507df66fb2f7a3a12",
+    "url": "/static/media/heroku-icon.cb5e5ee7.svg"
+  },
+  {
+    "revision": "10276fa2b5473fb1533379af3f754af6",
+    "url": "/static/media/javascript-icon.10276fa2.svg"
+  },
+  {
+    "revision": "919e347d2980cccf043e3d56e00fac34",
+    "url": "/static/media/linkedin.919e347d.svg"
+  },
+  {
+    "revision": "4a076edf56a6bbeaef44787d9aafe402",
+    "url": "/static/media/mail.4a076edf.svg"
+  },
+  {
+    "revision": "413385d067331bc9b71d6d49c84ba179",
+    "url": "/static/media/menu-bttn.413385d0.svg"
+  },
+  {
+    "revision": "ec3b49f078cfa4cdb57295b279ede5dd",
+    "url": "/static/media/mysql-icon.ec3b49f0.svg"
+  },
+  {
+    "revision": "c93c740cf03e22063e76151a55d41482",
+    "url": "/static/media/node-icon.c93c740c.svg"
+  },
+  {
+    "revision": "685cc4cab34cb8d69322be4abb0bdfef",
+    "url": "/static/media/php-icon.685cc4ca.svg"
+  },
+  {
+    "revision": "61854ef2c2e81e45b950afab47941371",
+    "url": "/static/media/postgresql-icon.61854ef2.svg"
+  },
+  {
+    "revision": "586b6439762e6a1e435e02cc5b26d23a",
+    "url": "/static/media/python-icon.586b6439.svg"
+  },
+  {
+    "revision": "6e390feb23b6d6d6a4256ccb627a3784",
+    "url": "/static/media/react-icon.6e390feb.svg"
+  },
+  {
+    "revision": "ecc8821f9d0b5602edb12ac10383599a",
+    "url": "/static/media/sortVisApp.ecc8821f.svg"
+  },
+  {
+    "revision": "2465cd23c318cbcf2c3eb0c015d93157",
+    "url": "/static/media/soundCanApp.2465cd23.svg"
+  },
+  {
+    "revision": "7e09095e480a1580f06ed351362f1fa5",
+    "url": "/static/media/soundCanvasIcon.7e09095e.svg"
+  },
+  {
+    "revision": "41f4f07e04a3b5f0c47f184416e32445",
+    "url": "/static/media/squareIcon.41f4f07e.svg"
+  },
+  {
+    "revision": "15f963e4dc09b733fb56edfec3cfb165",
+    "url": "/static/media/xd-icon.15f963e4.svg"
+  }
+]);
